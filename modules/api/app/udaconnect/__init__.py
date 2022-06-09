cedcd35d7be3efc0aa200970cda35ff4f1f51822
Hainan Chen <hainan.ch.chen@daimler.com>
2022-06-09 22:26:41 +0200

    foo

--- modules/api/app/udaconnect/__init__.py
+++ modules/api/app/udaconnect/__init__.py
@@ -2,13 +2,18 @@ from app.udaconnect.models import Connection, Location, Person  # noqa
 from app.udaconnect.schemas import ConnectionSchema, LocationSchema, PersonSchema  # noqa
 import os
 
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger("udaconnect-api")
+
 
 def register_routes(api, app, root="api"):
     if "APP" in os.environ and os.environ["APP"] == "location":
         from modules.api.app.udaconnect.controllers_location import api as udaconnect_location_api
         api.add_namespace(udaconnect_location_api, path=f"/{root}")
-        print("App Location started")
+        logging.INFO("App Location started")
     else:
         from modules.api.app.udaconnect.controllers_person import api as udaconnect_person_api
         api.add_namespace(udaconnect_person_api, path=f"/{root}")
-        print("App person started")
+        logging.INFO("App person started")
