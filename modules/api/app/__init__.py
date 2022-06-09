7a7eff7782013f270184c267dc74dca5de728d8f
Hainan Chen <hainan.ch.chen@daimler.com>
2022-06-09 22:48:48 +0200

    foo

--- modules/api/app/__init__.py
+++ modules/api/app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify
 from flask_cors import CORS
 from flask_restx import Api
 from flask_sqlalchemy import SQLAlchemy
+import os
 
 db = SQLAlchemy()
 
@@ -12,8 +13,10 @@ def create_app(env=None):
 
     app = Flask(__name__)
     app.config.from_object(config_by_name[env or "test"])
-    api = Api(app, title="UdaConnect API", version="0.1.0")
-
+    if "APP" in os.environ and os.environ["APP"] == "location":
+        api = Api(app, title="UdaConnect Location API", version="0.1.0")
+    else:
+        api = Api(app, title="UdaConnect Person API", version="0.1.0")
     CORS(app)  # Set CORS for development
 
     register_routes(api, app)
