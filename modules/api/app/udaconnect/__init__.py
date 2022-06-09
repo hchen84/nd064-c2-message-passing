0b2a92d2e1d489d1842328c9ef61792e867dbb47
Hainan Chen <hainan.ch.chen@daimler.com>
2022-06-09 22:32:19 +0200

    foo

--- modules/api/app/udaconnect/__init__.py
+++ modules/api/app/udaconnect/__init__.py
@@ -12,8 +12,8 @@ def register_routes(api, app, root="api"):
     if "APP" in os.environ and os.environ["APP"] == "location":
         from modules.api.app.udaconnect.controllers_location import api as udaconnect_location_api
         api.add_namespace(udaconnect_location_api, path=f"/{root}")
-        logging.INFO("App Location started")
+        logging.ERROR("App Location started")
     else:
         from modules.api.app.udaconnect.controllers_person import api as udaconnect_person_api
         api.add_namespace(udaconnect_person_api, path=f"/{root}")
-        logging.INFO("App person started")
+        logging.ERROR("App person started")
